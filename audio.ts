cc7b496d3292250d0b7941d311e45b3e830ae6cc
reccwb-del <reccwb@gmail.com>
2026-06-19 23:07:13 -0300

    Update audio.ts

--- audio.ts
+++ audio.ts
@@ -1,6 +1,8 @@
 export const playClickSound = () => {
-  // Substitua 'click.mp3' pelo nome exato do seu novo arquivo de som
-  const audio = new Audio('/assets/seu-novo-som.mp3'); 
+  const audio = new Audio('/assets/click.mp3'); 
   audio.volume = 0.8;
   audio.play().catch(e => console.log("Erro ao tocar som:", e));
 };
+
+export const isSoundEnabled = () => true;
+export const setSoundEnabled = (enabled: boolean) => {};
